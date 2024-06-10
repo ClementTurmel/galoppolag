1307209c3615d17e943800274aceb1acc42a0924
Clement TURMEL <clemturmel@gmail.com>
2024-06-10 15:00:25 +0200

    playing with views and templates

--- galoppolag/equines/views.py
+++ galoppolag/equines/views.py
@@ -1,16 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from equines.models import Person
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
 
 
 def index(request):
-    oldest_person = Person.objects.order_by("birthday")[:5]
-    output = ", ".join([f"{p.first_name} ({p.birthday.year})" for p in oldest_person])
-    return HttpResponse(output)
+    oldest_persons = Person.objects.order_by("birthday")[:5]
+    template = loader.get_template("equines/index.html")
+    context = {
+        "oldest_persons": oldest_persons,
+    }
+    return render(request, "equines/index.html", context)
 
 
 def equine_detail(request, equine_id):
     return HttpResponse(f"You're looking at equine {equine_id}")
 
 def owner_detail(request, equine_id):
-    return HttpResponse("You're looking at owner %s." % equine_id)
+    person = get_object_or_404(Person, pk=equine_id)
+    return render(request, "equines/person.html", {"person": person})
